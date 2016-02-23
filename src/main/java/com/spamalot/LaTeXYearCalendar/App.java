bb694e73a1538648910785cd482ea4d7ffcfc58d
Gene Johannsen <gene.johannsen@hpe.com>
2016-02-23 12:02:54 -0800

    New project checkin

---
+++ src/main/java/com/spamalot/LaTeXYearCalendar/App.java
@@ -0,0 +1,23 @@
+package com.spamalot.LaTeXYearCalendar;
+
+/**
+ * Hello world.
+ *
+ */
+public final class App {
+  /**
+   * Construct nothing.
+   */
+  private App() {
+  }
+
+  /**
+   * Do all the things.
+   * 
+   * @param args
+   *          The arguments to the program
+   */
+  public static void main(final String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/com/spamalot/LaTeXYearCalendar/AppTest.java
@@ -0,0 +1,36 @@
+package com.spamalot.LaTeXYearCalendar;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+  /**
+   * Create the test case.
+   *
+   * @param testName
+   *          name of the test case
+   */
+  public AppTest(final String testName) {
+    super(testName);
+  }
+
+  /**
+   * Test suite.
+   * 
+   * @return the suite of tests being tested
+   */
+  public static Test suite() {
+    return new TestSuite(AppTest.class);
+  }
+
+  /**
+   * Rigourous Test. :-)
+   */
+  public static void testApp() {
+    assertTrue(true);
+  }
+}
